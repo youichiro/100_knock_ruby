5963cb7ec563071d33fee8d2a63e5877d1e55a49
youichiro <cinnamon416@gmail.com>
2017-12-15 00:49:54 +0900

    スペースにする

--- chap01/q05.rb
+++ chap01/q05.rb
@@ -1,30 +1,30 @@
 # encoding: utf-8
 def word_ngram(s, n)
-	words = s.split(' ')
-	hash = {}
-	hash.default = 0
-	for i in 0..words.length-n do
-		ngram = []
-		for j in 0..n-1 do
-			ngram.push(words[i+j])
-		end
-		hash[ngram.join(' ')] += 1
-	end
-	return hash
+    words = s.split(' ')
+    hash = {}
+hash.default = 0
+    for i in 0..words.length-n do
+        ngram = []
+        for j in 0..n-1 do
+            ngram.push(words[i+j])
+        end
+        hash[ngram.join(' ')] += 1
+    end
+    return hash
 end
 
 def char_ngram(s, n)
-	chars = s.gsub(/ /, '').split('')
-	hash = {}
-	hash.default = 0
-	for i in 0..chars.length-n do
-		ngram = []
-		for j in 0..n-1 do
-			ngram.push(chars[i+j])
-		end
-		hash[ngram.join(' ')] += 1
-	end
-	return hash
+    chars = s.gsub(/ /, '').split('')
+    hash = {}
+    hash.default = 0
+    for i in 0..chars.length-n do
+        ngram = []
+        for j in 0..n-1 do
+            ngram.push(chars[i+j])
+        end
+        hash[ngram.join(' ')] += 1
+    end
+    return hash
 end
 
 s = 'I am an NLPer'
