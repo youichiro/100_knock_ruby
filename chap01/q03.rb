c105e8044412d2923aff9e403b79d1b9d7e5cd41
youichiro <cinnamon416@gmail.com>
2017-12-14 23:39:08 +0900

    fix chap01/q03

--- chap01/q03.rb
+++ chap01/q03.rb
@@ -1,5 +1,9 @@
 # encoding: utf-8
 s = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
-words = s.split(' ')
-arr = words.map { |w| w.length }
-print arr
+words = s.split(' ').each {|w|
+	w.gsub!(/[,.]/, '')
+}
+arr = words.map {|w|
+    w.length
+}
+print(arr)
