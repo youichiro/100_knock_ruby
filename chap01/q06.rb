3541f34b937a1ef0d7d6522965101798b8e74edc
youichiro <cinnamon416@gmail.com>
2017-12-16 02:20:41 +0900

    fix chap01/q06

--- chap01/q06.rb
+++ chap01/q06.rb
@@ -6,6 +6,20 @@ s2 = 'paragraph'
 ngram1 = char_ngram(s1, 2)
 ngram2 = char_ngram(s2, 2)
 
-print "和集合 ", ngram1.keys | ngram2.keys, "\n"
-print "差集合 ", ngram1.keys - ngram2.keys, "\n"
-print "積集合 ", ngram1.keys & ngram2.keys, "\n"
+print "X: ", ngram1.keys, "\n"
+print "Y: ", ngram2.keys, "\n"
+print "和集合: ", ngram1.keys | ngram2.keys, "\n"
+print "差集合: ", ngram1.keys - ngram2.keys, "\n"
+print "積集合: ", ngram1.keys & ngram2.keys, "\n"
+
+if ngram1.keys.include?("se") then
+    puts "Xにseが含まれる"
+else
+    puts "Xにseが含まれない"
+end
+
+if ngram2.keys.include?("se") then
+    puts "Yにseが含まれる"
+else
+    puts "Yにseが含まれない"
+end
